d6726c0de2b3563fe1c942be47aa52c5b00a2509
monankp1 <monankp1@gmail.com>
2024-04-02 00:40:11 +0530

    intial commit

--- src/Form.jsx
+++ src/Form.jsx
@@ -78,7 +78,7 @@ const Form = () => {
         <h1>User Details Modal</h1>
         <button onClick={handleForm}>Open Form</button>
         <div style={overlayStyle} onClick={() => handleClose()}></div>
-            {!isVisible && initial && <div className='modal'></div>}
+            {initial && <div className='modal'></div>}
             {isVisible && (
                 <div className='modal'>
                 <div className='modal-content'>
